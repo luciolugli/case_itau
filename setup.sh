6812c311bd8a17d9bfc68c8175f56bbadde9b6a0
luciolugli <luciolugli@gmail.com>
2024-05-27 10:39:14 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -34,19 +34,19 @@ pip install flask pyjwt flask-swagger-ui
 mkdir -p static
 touch static/swagger.yaml
 
+
 # Adiciona o conteúdo do swagger.yaml
 cat <<EOT > static/swagger.yaml
 openapi: 3.0.0
 info:
-  title: JWT Verifier API
   version: 1.0.0
-servers:
-  - url: http://localhost:5000
+  title: Api Validador JWT
+  description: API para verificação de tokens JWT
 paths:
   /verify:
     post:
-      summary: Verify JWT
-      operationId: verifyJwt
+      summary: Verifica se um token JWT é válido
+      description: Verifica se um token JWT atende a determinadas regras.
       requestBody:
         required: true
         content:
@@ -56,10 +56,10 @@ paths:
               properties:
                 token:
                   type: string
-                  description: JWT token to verify
+                  description: Token JWT a ser verificado
       responses:
         '200':
-          description: JWT verification result
+          description: Sucesso na verificação do token
           content:
             application/json:
               schema:
@@ -67,9 +67,15 @@ paths:
                 properties:
                   message:
                     type: string
-                    description: JWT verification message
+                    description: Mensagem de sucesso
+                  justification:
+                    type: string
+                    description: Justificativa para o sucesso da verificação do token
+                  payload:
+                    type: object
+                    description: Payload do token JWT
         '400':
-          description: Bad request
+          description: Erro de solicitação inválida
           content:
             application/json:
               schema:
@@ -77,9 +83,9 @@ paths:
                 properties:
                   error:
                     type: string
-                    description: Error message
-        '500':
-          description: Internal server error
+                    description: Mensagem de erro
+        '401':
+          description: Token JWT inválido
           content:
             application/json:
               schema:
@@ -87,16 +93,32 @@ paths:
                 properties:
                   error:
                     type: string
-                    description: Error message
+                    description: Mensagem de erro
+components:
+  schemas:
+    ErrorResponse:
+      type: object
+      properties:
+        error:
+          type: string
+          description: Mensagem de erro
+servers:
+  # Added by API Auto Mocking Plugin
+  - description: SwaggerHub API Auto Mocking
+    url: https://virtserver.swaggerhub.com/LUCIOLUGLI/jwt-api/1.0.0
+  - url: http://127.0.0.1:5000
+    description: Local development server
+
 EOT
 
 # Adiciona o conteúdo do app.py
-cat <<EOT > app.py
 from flask import Flask, request, jsonify
 import jwt
 from jwt.exceptions import InvalidTokenError
 import math
 from flask_swagger_ui import get_swaggerui_blueprint
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -122,42 +144,71 @@ def is_prime(n):
         i += 6
     return True
 
+# Configuração do logger
+def configure_logging(app):
+    # Configuração do logger principal
+    app.logger.setLevel(logging.DEBUG)
+
+    # Formato das mensagens de log
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Handler para log para console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)  # Defina o nível do log conforme necessário
+    console_handler.setFormatter(formatter)
+    app.logger.addHandler(console_handler)
+
+    # Handler para log em arquivo rotativo
+    file_handler = RotatingFileHandler('app.log', maxBytes=1024000, backupCount=10)
+    file_handler.setLevel(logging.DEBUG)  # Defina o nível do log conforme necessário
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
+# Chamada para configurar o logging
+configure_logging(app)
+
 @app.route('/verify', methods=['POST'])
 def verify_jwt():
     token = request.json.get('token')
     if not token:
-        return jsonify({'error': 'Token is required'}), 400
+        return jsonify({'error': 'Token obrigatorio'}), 400
 
     try:
         payload = jwt.decode(token, options={"verify_signature": False})
     except InvalidTokenError:
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'JWT invalido'}), 200
 
     required_claims = ['Name', 'Role', 'Seed']
     if len(payload) != 3 or any(claim not in payload for claim in required_claims):
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'Foi encontrado mais de 3 claims.'}), 400
 
     name = payload['Name']
     role = payload['Role']
     seed = payload['Seed']
 
     if not isinstance(name, str) or any(char.isdigit() for char in name) or len(name) > 256:
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'Claim Name invalido. Abrindo o JWT, a Claim Name possui caracter de numeros'}), 400
 
     if role not in ['Admin', 'Member', 'External']:
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'Role Claim Role Invalido'}), 400
 
     try:
         seed = int(seed)
     except ValueError:
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'Seed deve ser um numero'}), 400
 
     if not is_prime(seed):
-        return jsonify({'message': 'JWT is invalid'}), 200  # Responde com sucesso, mas indica que o JWT é inválido
+        return jsonify({'error': 'A claim Seed deve ser um numero primo'}), 400
 
-    return jsonify({'message': 'JWT is valid'}), 200
+    # Se todas as verificações passarem, retornar uma resposta de sucesso com detalhes do payload e justificativa
+    return jsonify({
+        'message': 'JWT valido',
+        'justification': 'Abrindo o JWT, as informações contidas atendem a descricao',
+        'payload': payload
+    }), 200
 
 if __name__ == '__main__':
+    configure_logging(app)
     app.run(debug=True)
 EOT
 
